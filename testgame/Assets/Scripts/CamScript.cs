39fa0a1163d3ae6dd5a8e8f4fda72252dbbae439
yun0jae <geniusyun98@naver.com>
2023-06-02 01:04:23 +0900

    Delete ArtifactDB
    edit material asset
    
    edit material asset
    
    edit material asset
    
    edit material asset

--- testgame/Assets/Scripts/CamScript.cs
+++ testgame/Assets/Scripts/CamScript.cs
@@ -5,17 +5,10 @@ using UnityEngine;
 public class CamScript : MonoBehaviour
 {
     GameObject player;
-    GameManager gameManager;
-    WorldREffectScript effectScript;
-
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.Find("Player");
-        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
-
-        effectScript = GameObject.Find("Main Camera").GetComponent<WorldREffectScript>();
-        effectScript.enabled = false;
     }
 
     // Update is called once per frame
@@ -24,15 +17,6 @@ public class CamScript : MonoBehaviour
         Vector3 campos = this.transform.position;
         transform.position = new Vector3(player.transform.position.x, campos.y, player.transform.position.z);
 
-        if (gameManager.stages[1].activeSelf)
-        {
-            effectScript.enabled = true;
-        }
-        else
-        {
-            effectScript.enabled = false;
-        }
-
-
+        
     }
 }
--- testgame/Assets/Scripts/PlayerMove.cs
+++ testgame/Assets/Scripts/PlayerMove.cs
@@ -2,15 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using static System.Net.Mime.MediaTypeNames;
 
 public class PlayerMove : MonoBehaviour
 {
     GameManager gameManager;
-    public GameObject player;
-    public WorldRScript playerMoveR;
-    public WorldBScript playerMoveB;
-    public GameObject Object;
+    AudioSource audioSource;
+    public GameObject player, people, Object, text;
+    
+
+    public float playerSpeed = 10.0f;
+    public bool manual_ver = false;
+    public bool goal = false;
+    bool onGround = true;
+    bool objectR = false;
+
+    public float time; // 자막 없어지는 타이밍 정하기 위한 변수
 
 
     Rigidbody rigid;
@@ -18,82 +24,151 @@ public class PlayerMove : MonoBehaviour
     private void Awake()
     {
         gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
-        player = GameObject.Find("player");
+        people = GameObject.Find("People");
 
-        playerMoveR = GameObject.Find("WorldR").GetComponent<WorldRScript>();
-        playerMoveB = GameObject.Find("WorldB").GetComponent<WorldBScript>();
-
+        Object = GameObject.Find("object"); //SetActive 활용을 위한 보석 오브젝트 호출
+        player = GameObject.Find("player"); //player 좌표를 측정하기 위해 오브젝트 호출
+        text = GameObject.Find("Text (Legacy)"); //text 컨트롤을 위해 오브젝트 호출
     }
 
+    // Start is called before the first frame update
     void Start()
     {
+        rigid = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+        people.transform.position = new Vector3(-30, 2, 1); //모델링 때문에 틀어진 좌표 보정
+    }
+
+    void FixedUpdate()
+    {
+
+        if (gameManager.stages[0].activeSelf)
+        {
+            bool jumpInput;
+            jumpInput = Input.GetButtonDown("Jump");
+            if (onGround & jumpInput) rigid.AddForce(Vector3.up * 20, ForceMode.Impulse);
+            if (rigid.velocity.y > 0 & rigid.position.y > 15) rigid.AddForce(Vector3.up * (-20), ForceMode.Impulse);
+            
+        }
 
     }
 
+    // Update is called once per frame
     void Update()
     {
 
-
-    }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        //BLUE
         if (gameManager.stages[0].activeSelf)
         {
-            if (collision.transform.CompareTag("Goal"))
+            if (objectR)
             {
-                playerMoveB.goal = true;
-                Debug.Log("BLUE goal reached");
+                manual_ver = false;
+                people.GetComponent<Animator>().Play("Run"); //쫓아오는 애니메이션 시작
+                people.transform.Translate(0, 0, playerSpeed * Time.deltaTime);
+               
+
+            }
+            if (goal)
+            {
+                objectR = false;
+                manual_ver = true;
+                if (Input.GetButtonDown("Fire1")) gameManager.GoNextStage();
+                gameObject.GetComponent<EffectScript>().enabled = false;
+            }
+
+            time += Time.deltaTime; // 자막 없애는 타이밍 구현
+            if (time > 10)
+            {
+                text.SetActive(false); 
             }
         }
 
-            //RED
-            if (gameManager.stages[1].activeSelf)
+        //Horizontal movement
+        int h = (int)Input.GetAxisRaw("Horizontal");
+
+        Vector3 velo = Vector3.zero;
+        Vector3 target = new Vector3(transform.position.x,
+                                    transform.position.y,
+                                    transform.position.z - 5 * h);
+        transform.position = Vector3.SmoothDamp(transform.position, target, ref velo, 0.1f);
+
+        //Vertical Movement
+        int v = (int)Input.GetAxisRaw("Vertical");
+        if (!manual_ver)
+        {
+            transform.Translate(playerSpeed*(1+v/2) * Time.deltaTime, 0, 0);
+        }
+        else
+        {
+            transform.position += new Vector3(playerSpeed, 0.0f, 0.0f) * Time.deltaTime * v;
+        }
+
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.CompareTag("goal"))
+        {
+            audioSource.Stop();
+            goal = true;
+            Debug.Log("goal reached!");
+        }
+
+        if (gameManager.stages[0].activeSelf)
         {
             if (collision.transform.CompareTag("Object"))
             {
-                playerMoveR.objectR = true;
-                Debug.Log("RED object gained");
+                objectR = true;
+                Object.SetActive(false); //오브젝트 획득하면 사라짐
+                text.GetComponent<Text>().text = "장애물을 피해서 앞으로 달리세요!"; //텍스트 내용 변경
+                audioSource.Play();
+                Debug.Log("object gained");
             }
 
-            if (collision.transform.CompareTag("Goal"))
+            if (collision.transform.CompareTag("obstacle"))
             {
-                playerMoveR.goal = true;
-                Debug.Log("RED goal reached");
+                Debug.Log(collision.gameObject.name);
             }
 
             if (collision.transform.CompareTag("Reset"))
             {
-                playerMoveR.InitRound();
+                Object.SetActive(true); //게임오버하면 재생성
+                text.GetComponent<Text>().text = "보석을 획득하세요!"; //텍스트 원상복구
+                InitPosition();
             }
+
+            if (collision.transform.CompareTag("Floor"))
+            {
+                onGround = true;
+            }
+
         }
-            
 
     }
-
 
     private void OnCollisionStay(Collision collision)
     {
-        //BLUE
-        if (gameManager.stages[0].activeSelf)
-        {
-            if (collision.transform.CompareTag("Object"))
-            {
-                playerMoveB.objectB = true;
-                Debug.Log("BLUE object gained");
-            }
-        }
-
-        //RED
-        if (gameManager.stages[1].activeSelf)
-        {
-            playerMoveR.onGround = collision.transform.CompareTag("Floor");
-
-        }
-
-
-        
+        onGround = collision.transform.CompareTag("Floor");
     }
 
+
+    public void InitPosition()
+    {
+        transform.position = new Vector3(0.0f, 8.0f, 0.0f);
+
+        if (gameManager.stages[1].activeSelf)
+        {
+            manual_ver = false;
+        }
+
+        if (gameManager.stages[0].activeSelf)
+        {
+            manual_ver = true;
+            goal = false;
+            onGround = true;
+            objectR = false;
+            people.transform.position = new Vector3(-40.0f, 8.0f, 0.0f);
+        }
+
+    }
 }
